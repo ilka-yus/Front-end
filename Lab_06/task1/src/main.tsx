9ab0bfdf3bc6519a2340600eb1a18055e96bf930
ilka-yus <ilnara.u06@gmail.com>
2026-02-21 08:55:06 +0500

    task1

---
+++ Lab_06/task1/src/main.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
+const Courses = () => <h2>Courses Page</h2>;
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'courses', element: <Courses /> },
+      { path: 'about', element: <About /> },
+      { path: '*', element: <NotFound />},
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ Lab_06/task1/src/pages/About.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+    return <p>This program is designed to help students master React Router.</p>;
+}
